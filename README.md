747ae68739aadd97433208616d92b4c7fff893ed
dipesh <dipesh.acharya@gmail.com>
2012-08-25 15:53:33 +0900

    version 0.1

---
+++ README.md
@@ -0,0 +1,2 @@
+A simple (really simple) blog site based on nodejs & mongoosejs. CSS stylesheet is powered by Twitter bootstrap,
+and for Javascript ubiquitous jQuery. 
--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, '/public')));
+  app.use(express.favicon(__dirname + '/public/images/favicon.ico',{ maxAge: 2592000000 }));
 });
 
 app.configure('development', function(){
